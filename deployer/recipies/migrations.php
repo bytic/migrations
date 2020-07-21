6b1beb6b9e3db87622ca4fc8dbf2fed417b99fe6
Gabriel Solomon <hello@gabrielsolomon.ro>
2020-07-21 14:48:39 +0300

    update migration phinx

--- deployer/recipies/migrations.php
+++ deployer/recipies/migrations.php
@@ -2,8 +2,15 @@
 
 namespace Deployer;
 
-require 'recipe/phinx.php';
-
 /*** SCHEDULER PUBLISH***/
 desc('Run migrations');
-task('bytic:migrations:migrate', 'phinx:migrate');
+task('bytic:migrations:migrate', function () {
+    cd('{{release_path}}');
+
+    $bytic = get('bin/bytic');
+    $byticCmd = "$bytic migrations:migrate";
+
+    run($byticCmd);
+
+    cd('{{deploy_path}}');
+});
